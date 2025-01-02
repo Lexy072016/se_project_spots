e14fab34d1bf981f8fb409cb3e1abe89a4d68e07
Lexy <lexy221596@yahoo.com>
2025-01-01 18:08:47 -0800

    {update}

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: "City along the river",
+    link: "C:Userslexy2se_project_spotsimagescity-along-the-river.jpg",
+  },
+  {
+    name: "Mountain range",
+    link: "C:Userslexy2se_project_spotsimagesmountain-range.jpg",
+  },
+  {
+    name: "Night sky",
+    link: "C:Userslexy2se_project_spotsimages\night-sky.jpg",
+  },
+  {
+    name: "Sea rocks",
+    link: "C:Userslexy2se_project_spotsimagessea-rocks.jpg",
+  },
+  {
+    name: "Snowy park",
+    link: "C:Userslexy2se_project_spotsimagessnowy-park.jpg",
+  },
+  {
+    name: "Waterfall",
+    link: "C:Userslexy2se_project_spotsimageswaterfall.jpg",
+  },
+];
